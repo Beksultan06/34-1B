87182545c57ba7d30bd9d6714e4a48294d57f51d
beksultan <nurlanuuulubeksultan@gmail.com>
2026-03-09 19:55:40 +0600

    fix comment

--- app/product/views.py
+++ app/product/views.py
@@ -4,11 +4,17 @@ from rest_framework.generics import (
     UpdateAPIView, DestroyAPIView   
 )
 
+from rest_framework import mixins, filters
+from rest_framework.viewsets import GenericViewSet
+from django_filters.rest_framework import DjangoFilterBackend
+
 from app.product.serializers import (
     CategorySerializers, TypesSerilaizers,
     ProductSerializer, ProductCreateSerializers
 )
 from app.product.models import Category, Types, Product
+from app.paginatios import Pagination
+from app.filters import ProductFilter
 
 class CategoryAPIView(ListAPIView):
     queryset = Category.objects.all()
@@ -18,40 +24,66 @@ class TypesAPIView(ListAPIView):
     queryset = Types.objects.all()
     serializer_class = TypesSerilaizers
 
-class ProductAPIView(ListAPIView):
-    queryset = Product.objects.all().prefetch_related("images")
-    serializer_class = ProductSerializer
-
-    def get_serializer_context(self):
-        ctx = super().get_serializer_context()
-        ctx["reuqest"] = self.request
-        return ctx
-
-class ProductCreateAPIView(CreateAPIView):
+class ProductAPI(mixins.ListModelMixin, 
+                mixins.CreateModelMixin,
+                mixins.UpdateModelMixin,
+                mixins.RetrieveModelMixin,
+                mixins.DestroyModelMixin,
+                GenericViewSet):
     queryset = Product.objects.all()
-    serializer_class = ProductCreateSerializers
+    pagination_class = Pagination
+    filter_backends = [
+        DjangoFilterBackend, 
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+    filterset_class = ProductFilter
+    search_fields = ['title', 'description']
+    ordering_fields = ['id', 'price', 'created_at']
+    
+    def get_serializer_class(self):
+        if self.action in ["list", "retrieve"]:
+            return ProductSerializer
+        if self.action in ["create", "update", "partial_update"]:
+            return ProductCreateSerializers
+        return ProductSerializer
 
-class ProductRetrieveAPIView(RetrieveAPIView):
-    queryset = Product.objects.all().prefetch_related("images")
-    serializer_class = ProductSerializer
+    
 
-    lookup_field = "uuid"
-    lookup_url_kwarg = "uuid"
-
-    def get_serializer_context(self):
-        ctx = super().get_serializer_context()
-        ctx["reuqest"] = self.request
-        return ctx
-
-class ProductUpdateAPIView(UpdateAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductCreateSerializers
-
-    lookup_field = "uuid"
-    lookup_url_kwarg = "uuid"
-
-class ProductDeleteAPIView(DestroyAPIView):
-    queryset = Product.objects.all()
-
-    lookup_field = "uuid"
-    lookup_url_kwarg = "uuid"
+# class ProductAPIView(ListAPIView):
+#     queryset = Product.objects.all().prefetch_related("images")
+#     serializer_class = ProductSerializer
+# 
+#     def get_serializer_context(self):
+#         ctx = super().get_serializer_context()
+#         ctx["reuqest"] = self.request
+#         return ctx
+# 
+# class ProductCreateAPIView(CreateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductCreateSerializers
+# 
+# class ProductRetrieveAPIView(RetrieveAPIView):
+#     queryset = Product.objects.all().prefetch_related("images")
+#     serializer_class = ProductSerializer
+# 
+#     lookup_field = "uuid"
+#     lookup_url_kwarg = "uuid"
+# 
+#     def get_serializer_context(self):
+#         ctx = super().get_serializer_context()
+#         ctx["reuqest"] = self.request
+#         return ctx
+# 
+# class ProductUpdateAPIView(UpdateAPIView):
+#     queryset = Product.objects.all()
+#     serializer_class = ProductCreateSerializers
+# 
+#     lookup_field = "uuid"
+#     lookup_url_kwarg = "uuid"
+# 
+# class ProductDeleteAPIView(DestroyAPIView):
+#     queryset = Product.objects.all()
+# 
+#     lookup_field = "uuid"
+#     lookup_url_kwarg = "uuid"
--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     #rest
     "rest_framework",
+    'django_filters',
 
     #app
     'app.product', 
@@ -130,3 +131,11 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend",
+        "rest_framework.filters.SearchFilter",
+        "rest_framework.filters.OrderingFilter",
+    ],
+}
